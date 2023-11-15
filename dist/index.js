285bb2961ae6c62a53a485a8509b93109ab48658
carlos11401 <carlosjavier.14.cjcg@gmail.com>
2023-11-14 20:17:09 -0600

    debugging

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,6 @@
 "use strict";
 console.log('Hello World!!!!');
 let message = 'Hello World';
+console.log(message);
+message = 'Hello World again';
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
 console.log('Hello World!!!!');
 let message : string = 'Hello World';
+
+console.log(message);
+
+message = 'Hello World again';
